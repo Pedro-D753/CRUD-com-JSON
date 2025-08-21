43d2c7c853189498119312905c24205b07a17b95
Pedro Domiense dos Santos <pedro.domiense05@gmail.com>
2025-08-21 12:26:03 -0300

    aula 5 e json

---
+++ aula05/app2.py
@@ -0,0 +1,34 @@
+import os
+
+while True:
+    try:
+        #usuario informa o nome do arquivo
+        arquivo = input('Digite o nome do arquivo (sem extensão):').strip.lower()
+
+        #abre o arquivo
+        with open(f'{arquivo}.txt', 'r', encoding = 'utf-8') as f:
+            arquivo_aberto = f.read()
+
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            # mostra os dados do arquivo
+            
+            print(arquivo_aberto)
+
+
+            while True:
+                 prosseguir = input('Deseja abrir outro arquivo?')
+                 if prosseguir == 's' or prosseguir == 'n':
+                  break
+                 else:
+                    print('Opção Inválida!')
+                    continue
+    match prosseguir:
+     case 's':
+        continue
+     case 'n':
+        break
+except Exception as e:
+print(f'Não foi possível ler o arquivo. {e}')
+
+   
---
+++ aula05/app3.py
@@ -0,0 +1,30 @@
+import os
+
+while True:
+    try:
+        novo_texto=input('Digite o texto: \n')
+        nome_arquivo = input('Digite o nome do arquivo (sem extensão):').strip().lower()
+
+        with open(fr'C:\Users\PedroDomiensedosSant\Documents\LogicaProgramaca_oAulas\aula05/{nome_arquivo}.txt', 'w', encoding= 'utf-8') as f:
+            f.write(novo_texto)
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f'{nome_arquivo}.txt gravado com sucesso.')
+
+        while True:
+            prosseguir = input('Deseja abrir outro arquivo? (S/N): ').strip().lower()
+            if prosseguir == 's' or prosseguir == 'n':
+                break
+            else:
+                print('Opção inválida!')
+                continue
+        match prosseguir:
+            case 's':
+                continue
+            case 'n':
+                 break
+            
+    except Exception as e :
+        print(f'Não foi ler o arquivo. {e}')
+        continue
+
